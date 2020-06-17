760f38934eee2917f2b22f7dda3b608a9d8593a8
Chris James <1523286+cajames@users.noreply.github.com>
2020-06-18 01:45:37 +1000

    fix: add missing translations (#3)

--- i18n/en.php
+++ i18n/en.php
@@ -3,6 +3,8 @@
 return [
     'editor.blocks.gallery.settings'                   => 'Gallery Settings',
     'editor.blocks.gallery.settings.group.label'       => 'Gallery Group Name',
-    'editor.blocks.gallery.settings.imageAlt.label'    => 'Gallery Alt Name',
-    'editor.blocks.gallery.block.addRow'    => 'Add a new row',
+    'editor.blocks.gallery.settings.addImageToRow'     => 'Add images to this row',
+    'editor.blocks.gallery.block.addRow'               => 'Add a new row',
+    'editor.blocks.gallery.block.uploadImages'         => 'Upload images',
+    'editor.blocks.gallery.block.selectImages'         => 'Select images'
 ];
