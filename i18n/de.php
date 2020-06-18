630ed956eb3f061d3e8cfa3e5bd6bc44a6cf1c6b
Chris James <chris.james@oovvuu.com>
2020-06-18 12:59:55 +1000

    fix: add translations for block label and other missing settings

--- i18n/de.php
+++ i18n/de.php
@@ -1,9 +1,11 @@
 <?php
 
 return [
+    'editor.blocks.gallery.label'                      => 'Galerie',
     'editor.blocks.gallery.settings'                   => 'Galerie Einstellungen',
     'editor.blocks.gallery.settings.group.label'       => 'Galerie Gruppenname',
     'editor.blocks.gallery.settings.addImageToRow'     => 'Bilder zu Zeile hinzufügen',
+    'editor.blocks.gallery.block.or'                   => 'oder',
     'editor.blocks.gallery.block.addRow'               => 'Zeile hinzufügen',
     'editor.blocks.gallery.block.uploadImages'         => 'Bilder hochladen',
     'editor.blocks.gallery.block.selectImages'         => 'Bilder auswählen'
